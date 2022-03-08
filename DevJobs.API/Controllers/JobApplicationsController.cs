4a3f414920ce76af7e491b60efe7d282a3702a9a
Vinicius Parente <vd.parente@hotmail.com>
2022-03-08 19:09:46 -0300

    Created Controllers Base API EndPoint

---
+++ DevJobs.API/Controllers/JobApplicationsController.cs
@@ -0,0 +1,19 @@
+﻿namespace DevJobs.API.Controllers
+{
+    using DevJobs.API.Entities;
+    using DevJobs.API.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/job-vacancies/{id}/applications")]
+    [ApiController]
+    public class JobApplicationsController : ControllerBase
+    {
+
+        // POST api/job-vacancies/4/applications
+        [HttpPost]
+        public IActionResult Post(int id, AddJobApplicationInputModel model)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevJobs.API/Controllers/JobVacanciesController.cs
@@ -0,0 +1,42 @@
+﻿namespace DevJobs.API.Controllers
+{
+    using DevJobs.API.Entities;
+    using DevJobs.API.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/job-vacancies")]
+    [ApiController]
+    public class JobVacanciesController : ControllerBase
+    {
+
+        // GET api/job-vacancies
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        // GET api/job-vacancies/4
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            //se nao existe e return NotFound();
+            return Ok();
+        }
+
+        // POST api/job-vacancies
+        [HttpPost]
+        public IActionResult Post(AddJobVacancyInputModel model)
+        {
+
+            return CreatedAtAction("GetById", new { id = 1 }, model);
+        }
+
+        // PUT api/job-vacancies/4
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UpdateJobVacancyInputModel model)
+        {
+            return NoContent();
+        }
+    }
+}
