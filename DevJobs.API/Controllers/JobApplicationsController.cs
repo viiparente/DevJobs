308f62775758576651d7386fae8bdcf08980e375
Vinicius Parente <vd.parente@hotmail.com>
2022-03-08 19:16:12 -0300

    Implement controller Context in Memory

--- DevJobs.API/Controllers/JobApplicationsController.cs
+++ DevJobs.API/Controllers/JobApplicationsController.cs
@@ -2,6 +2,7 @@
 {
     using DevJobs.API.Entities;
     using DevJobs.API.Models;
+    using DevJobs.API.Persistence;
     using Microsoft.AspNetCore.Mvc;
 
     [Route("api/job-vacancies/{id}/applications")]
@@ -9,10 +10,30 @@
     public class JobApplicationsController : ControllerBase
     {
 
+        private readonly DevJobsContext _context;
+        public JobApplicationsController(DevJobsContext context)
+        {
+            _context = context;
+        }
+
         // POST api/job-vacancies/4/applications
         [HttpPost]
         public IActionResult Post(int id, AddJobApplicationInputModel model)
         {
+            var jobVacancy = _context.JobVacancies
+                .SingleOrDefault(jv => jv.Id == id);
+
+            if (jobVacancy == null)
+                return NotFound();
+
+            var application = new JobApplication(
+                model.ApplicantName,
+                model.ApplicantEmail,
+                id
+            );
+
+            jobVacancy.Applications.Add(application);
+
             return NoContent();
         }
     }
